539a6ae40ebc53e49cc3ec301e24833e3a641e2b
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-04-15 18:01:50 -0400

    created store js file

---
+++ client/src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux'
+import reducers from './reducer'
+
+let preloadedState
+const persistedTodosString = localStorage.getItem('todos')
+
+if (persistedTodosString) {
+  preloadedState = {
+    todos: JSON.parse(persistedTodosString)
+  }
+}
+
+const store = createStore(rootReducer, preloadedState)
+
+export default store;
