58f09ac2de14388590ff0235ebeb22f71ab49b73
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-04-15 23:39:57 -0400

    edited categorymenu

--- client/src/components/CategoryMenu/index.jsx
+++ client/src/components/CategoryMenu/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
+// import { useStoreContext } from '../../utils/GlobalState';
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
@@ -8,8 +8,11 @@ import {
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
+
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
 
   const { categories } = state;
 
