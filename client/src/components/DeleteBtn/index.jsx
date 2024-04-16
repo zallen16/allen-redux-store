e5dbea84a1f9559b6769d4ec6cb6bccaa29ec449
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-04-15 23:43:34 -0400

    edited delete button

--- client/src/components/DeleteBtn/index.jsx
+++ client/src/components/DeleteBtn/index.jsx
@@ -1,10 +1,9 @@
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
-function DeleteBtn(props) {
+function DeleteBtn(...props) {
   return (
-    <span {...props} role="button" tabIndex="0">
-      ✗
-    </span>
+    <Provider {...props} role="button" tabIndex="0"
+    />
   );
 }
 
